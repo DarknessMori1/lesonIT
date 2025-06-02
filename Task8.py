1560a147095c3390eb517e5153623670dfb0fedf
Maxim <65599889+Maxim76866@users.noreply.github.com>
2025-06-02 08:59:23 +0000

    Task 8 has been comlpleted

---
+++ Task8.py
@@ -0,0 +1,8 @@
+def cities_information(s: str) -> tuple:
+    return s, len(s)
+
+cities = input().split()
+d = {city: cities_information(city)[1] for city in cities}
+
+for city in sorted(d, key=d.get):
+    print(f"'{city}': {d[city]}")
